fe27f21237adf0ae0644bc59fc35648ccdb6ab07
Algan Ongun <alganunlu1896@gmail.com>
2022-05-19 16:04:57 +0300

    Added Sliding Over Time To The Sections

---
+++ timer.js
@@ -0,0 +1,9 @@
+class Timer {
+	#tick() {
+		console.error('Error usage of interface Timer explicitly deleted');
+	}
+
+	startTimer() {
+		console.error('Error usage of interface Timer explicitly deleted');
+	}
+}
