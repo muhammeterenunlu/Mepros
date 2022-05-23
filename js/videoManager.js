a718c87f656596bc1254b97072a3e43c4189d9da
Muhammet Eren ÜNLÜ <muhammeterenunlu@gmail.com>
2022-05-24 01:10:31 +0300

    Contact us and footer sections were added. Small changes were made.

--- js/videoManager.js
+++ js/videoManager.js
@@ -6,11 +6,9 @@ const playPause = () => {
   if (video.paused) {
     video.play();
     btn.className = 'far fa-pause-circle';
-    video.style.opacity = '.7';
   } else {
     video.pause();
     btn.className = 'far fa-play-circle';
-    video.style.opacity = '.3';
   }
 };
 
@@ -21,9 +19,8 @@ btn.addEventListener('click', () => {
 video.addEventListener('timeupdate', () => {
   console.log(video.currentTime, video.duration);
   const barWidth = video.currentTime / video.duration;
-  bar.style.width = `${barWidth * 100}%`;
+  bar.style.width = `${barWidth * 120}%`;
   if (video.ended) {
     btn.className = 'far fa-play-circle';
-    video.style.opacity = '.3';
   }
 });
