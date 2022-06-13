e78455eed1d2bd5d4c194e4774fc4f0720d59154
AlganOngun <alganunlu1896@gmail.com>
2022-06-13 10:25:30 +0300

    Completed Homepage English Translation

--- Languages/en.js
+++ Languages/en.js
@@ -14,6 +14,20 @@ enlang = {
   shading_system_solutions: "Shading System Solutions",
   shading_system_solutions_seemore: "See More",
 
+  catalogues: "Catalogues",
+  catalogue_furniture_solutions: "Furniture Solutions",
+  catalogue_lighting_system_solutions: "Lighting System Solutions",
+  catalogue_shading_system_solutions: "Shading System Solutions",
+
   about: "About Us",
 
+  about_us_story_heading: "Who Are We?",
+  about_us_story_paragraph: "ENGLISH ABOUT US STORY PARAGRAPH PLACEHOLDER",
+  about_us_vision_heading: "Our Mission and Vision",
+  about_us_vision_paragraph: "ENGLISH ABOUT US VISION PARAGRAPH PLACEHOLDER",
+
+  contact_us: "Contact Us",
+  contact_us_phone: "Phone",
+  contact_us_address: "Address",
+  contact_us_email: "email"
 }
