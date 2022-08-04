6e9c9ba4add70bf277933b23120addaeb059cf49
Muhammet Eren ÜNLÜ <muhammeterenunlu@gmail.com>
2022-08-04 17:08:15 +0300

    Main page is fully completed

--- Languages/en.js
+++ Languages/en.js
@@ -1,32 +1,43 @@
 enlang = {
-  hamburger_menu_home: "Home",
-  hamburger_menu_products: "Products",
-  hamburger_menu_about: "About",
-  hamburger_menu_contact: "Contact Us",
+  hamburger_menu_home: 'Home',
+  hamburger_menu_products: 'Products',
+  hamburger_menu_about: 'About',
+  hamburger_menu_contact: 'Contact Us',
 
-  products: "Products",
-  furniture_solutions: "Furniture Solutions",
-  furniture_solutions_seemore: "See More",
+  products: 'Products',
+  furniture_solutions: 'Furniture Solutions',
+  furniture_solutions_seemore: 'See More',
 
-  lighting_system_solutions: "Lighting System Solutions",
-  lighting_system_solutions_seemore: "See More",
+  lighting_system_solutions: 'Lighting System Solutions',
+  lighting_system_solutions_seemore: 'See More',
 
-  shading_system_solutions: "Shading System Solutions",
-  shading_system_solutions_seemore: "See More",
+  shading_system_solutions: 'Shading System Solutions',
+  shading_system_solutions_seemore: 'See More',
 
-  catalogues: "Catalogues",
-  catalogue_furniture_solutions: "Furniture Solutions",
-  catalogue_lighting_system_solutions: "Lighting System Solutions",
-  catalogue_shading_system_solutions: "Shading System Solutions",
+  mepros_catalogues_section_heading: 'Catalogues',
+  catalogue_furniture_solutions: 'Sofa Solutions',
+  catalogue_furniture_solutions1: 'Chair Solutions',
+  catalogue_lighting_system_solutions: 'Led Lighting Solutions',
+  catalogue_lighting_system_solutions1: 'Lighting Pole and Bollards Solutions',
+  catalogue_shading_system_solutions: 'Shading Systems Solutions',
 
-  about: "About Us",
-  about_us_story_heading: "Who Are We?",
-  about_us_story_paragraph: "ENGLISH ABOUT US STORY PARAGRAPH PLACEHOLDER",
-  about_us_vision_heading: "Our Mission and Vision",
-  about_us_vision_paragraph: "ENGLISH ABOUT US VISION PARAGRAPH PLACEHOLDER",
+  about: 'About Us',
+  about_us_story_heading: 'Who Are We?',
+  about_us_story_paragraph:
+    'As Mepros, we are a company that offers quality and real solutions in the fields of furniture, lighting and shading systems.',
+  about_us_vision_heading: 'Our Mission and Vision',
+  about_us_vision_paragraph:
+    'To be a pioneer, strong and respected company that grows thanks to the privileges we offer to our customers and business partners.',
 
-  contact_us: "Contact Us",
-  contact_us_phone: "Phone",
-  contact_us_address: "Address",
-  contact_us_email: "Email"
-}
+  contact_us: 'Contact Us',
+  contact_us_phone: 'Phone',
+  contact_us_address: 'Address',
+  contact_us_email: 'Email',
+
+  mepros_section_1_heading: 'The Best Solutions',
+  mepros_section_2_heading: 'Newly Released Trend Products',
+  mepros_section_3_heading: 'Furniture, Lighting and Shading Solutions',
+  mepros_section_4_heading: 'The Best Products',
+  mepros_section_5_heading: 'Quality and Much More Besides',
+  mepros_section_6_heading: 'High End Solutions',
+};
--- Languages/tr.js
+++ Languages/tr.js
@@ -1,33 +1,44 @@
 trlang = {
-  hamburger_menu_home: "Anasayfa",
-  hamburger_menu_products: "Ürünler",
-  hamburger_menu_about: "Hakkımızda",
-  hamburger_menu_contact: "İletişim",
+  hamburger_menu_home: 'Anasayfa',
+  hamburger_menu_products: 'Ürünler',
+  hamburger_menu_about: 'Hakkımızda',
+  hamburger_menu_contact: 'İletişim',
 
-  products: "Ürünler",
-  furniture_solutions: "Mobilyalar",
-  furniture_solutions_seemore: "Daha Fazla",
+  products: 'Ürünler',
+  furniture_solutions: 'Mobilyalar',
+  furniture_solutions_seemore: 'Daha Fazla',
 
-  lighting_system_solutions: "Aydınlatma Sistemleri",
-  lighting_system_solutions_seemore: "Daha Fazla",
+  lighting_system_solutions: 'Aydınlatma Sistemleri',
+  lighting_system_solutions_seemore: 'Daha Fazla',
 
-  shading_system_solutions: "Gölgelendirme Sistemleri",
-  shading_system_solutions_seemore: "Daha Fazla",
+  shading_system_solutions: 'Gölgelendirme Sistemleri',
+  shading_system_solutions_seemore: 'Daha Fazla',
 
-  catalogues: "Katalog",
-  catalogue_furniture_solutions: "Mobilyalar",
-  catalogue_lighting_system_solutions: "Aydınlatma Sistemleri",
-  catalogue_shading_system_solutions: "Gölgelendirme Sistemleri",
+  mepros_catalogues_section_heading: 'Kataloglar',
+  catalogue_furniture_solutions: 'Mobilya Çözümleri',
+  catalogue_furniture_solutions1: 'Sandalye Çözümleri',
+  catalogue_lighting_system_solutions: 'Led Aydınlatma Çözümleri',
+  catalogue_lighting_system_solutions1: 'Aydınlatma Direği ve Direk Çözümleri',
+  catalogue_shading_system_solutions: 'Gölgelendirme Sistemleri Çözümleri',
 
-  about: "Hakkımızda",
-  about_us_story_heading: "Biz Kimiz?",
-  about_us_story_paragraph: "TURKISH ABOUT US STORY PLACEHOLDER",
+  about: 'Hakkımızda',
+  about_us_story_heading: 'Biz Kimiz?',
+  about_us_story_paragraph:
+    'Biz Mepros olarak mobilya, aydınlatma ve gölgelendirme sistemleri alanlarında kaliteli ve gerçek çözümleri sunan bir firmayız.',
 
-  about_us_vision_heading: "Misyon ve Vizyonumuz",
-  about_us_vision_paragraph: "TURKISH ABOUT US VISION PARAGRAPH PLACEHOLDER",
+  about_us_vision_heading: 'Misyon ve Vizyonumuz',
+  about_us_vision_paragraph:
+    'Müşterilerimize ve iş ortaklarımıza sunduğumuz ayrıcalıklarla büyüyen, öncü, güçlü ve saygın bir şirket olmak.',
 
-  contact_us: "Bize Ulaşın",
-  contact_us_phone: "Telefon",
-  contact_us_address: "Adres",
-  contact_us_email: "Email"
-}
+  contact_us: 'İletişim',
+  contact_us_phone: 'Telefon',
+  contact_us_address: 'Adres',
+  contact_us_email: 'Email',
+
+  mepros_section_1_heading: 'En İyi Çözümler',
+  mepros_section_2_heading: 'Yeni Çıkan Trend Ürünler',
+  mepros_section_3_heading: 'Mobilya, Aydınlatma ve Gölgelendirme Çözümleri',
+  mepros_section_4_heading: 'En İyi Ürünler',
+  mepros_section_5_heading: 'Kalite ve Daha Fazlası',
+  mepros_section_6_heading: 'Son Teknoloji Çözümler',
+};
