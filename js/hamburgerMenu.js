5bac6c6cd8757274da38d8a7e67546bf79777855
AlganOngun <alganunlu1896@gmail.com>
2022-08-19 23:53:27 +0300

    Sidebar now gets closed when clicked outside or clicked a link

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -2,6 +2,16 @@ document
   .querySelector('.mepros-hamburger-menu')
   .addEventListener('click', toggleHamburgerMenu);
 
-function toggleHamburgerMenu() {
+function toggleHamburgerMenu(event) {
   document.querySelector('.mepros-container').classList.toggle('change');
+}
+
+document.onclick = function(event) {
+		console.log(event.target.classList)
+	if(!event.target.classList.contains('mepros-sidebar') &&
+		!event.target.classList.contains('mepros-hamburger-menu') &&
+		document.querySelector('.mepros-container').classList.contains('change')
+	) {
+  	document.querySelector('.mepros-container').classList.toggle('change');
+	}
 }
